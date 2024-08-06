aad8414c336ca2e718858e4044f94162d14feacd
YOSRI BEN HALIMA <yosri.benhalima@ept.ucar.tn>
2024-08-07 00:46:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygwalker as pyg
+import pandas as pd
+import streamlit.components.v1 as components
+import streamlit as st
+
+# Load the data
+df = pd.read_csv("path_to_your_csv_file.csv")
+
+# Adjust the width and title of the Streamlit page
+st.set_page_config(
+    page_title="Pygwalker Data Visualization App Using Streamlit",
+    layout="wide"
+)
+
+st.title("Pygwalker Data Visualization App Using Streamlit")
+
+# Generate the HTML of the PyGWalker object
+pyg_html = pyg.walk(df, return_html=True)
+
+# Embed the HTML into the Streamlit app
+components.html(pyg_html, height=1000, scrolling=True)
