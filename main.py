11e10531d4e977aeb6c93ef0f6b2d535c7aafea2
YOSRI BEN HALIMA <yosri.benhalima@ept.ucar.tn>
2024-08-08 17:55:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import pandas as pd
 import streamlit.components.v1 as components
 import streamlit as st
 
-# Load the data
-df = pd.read_csv("path_to_your_csv_file.csv")
-
 # Adjust the width and title of the Streamlit page
 st.set_page_config(
     page_title="Pygwalker Data Visualization App Using Streamlit",
@@ -14,8 +11,17 @@ st.set_page_config(
 
 st.title("Pygwalker Data Visualization App Using Streamlit")
 
-# Generate the HTML of the PyGWalker object
-pyg_html = pyg.walk(df, return_html=True)
+# File uploader for CSV file
+uploaded_file = st.file_uploader("Upload your CSV file", type="csv")
 
-# Embed the HTML into the Streamlit app
-components.html(pyg_html, height=1000, scrolling=True)
+if uploaded_file is not None:
+    # Load the data into a DataFrame
+    df = pd.read_csv(uploaded_file)
+
+    # Generate the HTML of the PyGWalker object
+    pyg_html = pyg.walk(df, return_html=True)
+
+    # Embed the HTML into the Streamlit app
+    components.html(pyg_html, height=1000, scrolling=True)
+else:
+    st.warning("Please upload a CSV file to proceed.")
